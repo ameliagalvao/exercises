e8db30f33186c060ee9348a4ebc081612da85a4f
Amélia Galvão <ameliamgalvao@gmail.com>
2022-11-02 09:43:38 -0300

    adapter

--- TrackDelivery/app/src/main/java/com/miigubymia/trackdelivery/storage/ListAdapter.kt
+++ TrackDelivery/app/src/main/java/com/miigubymia/trackdelivery/storage/ListAdapter.kt
@@ -1,24 +1,31 @@
 package com.miigubymia.trackdelivery.storage
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.miigubymia.trackdelivery.MainActivity
+import com.miigubymia.trackdelivery.R
 
-class ListAdapter: RecyclerView.Adapter<ListAdapter.ListViewHolder>() {
+class ListAdapter(val registers: ArrayList<String> = arrayListOf<String>()): RecyclerView.Adapter<ListAdapter.ListViewHolder>() {
 
     class ListViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
-
+        val tvFileName: TextView = itemView.findViewById(R.id.tvFileName)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-        TODO("Not yet implemented")
+        val view:View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_register_card,parent,false)
+        return ListViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        var currentFile:String = registers[position]
+        holder.tvFileName.text = currentFile
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return registers.size
     }
 }
