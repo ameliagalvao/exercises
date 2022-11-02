e6d96116b215e717f745d39d8a4fb0399bd895a8
Amélia Galvão <ameliamgalvao@gmail.com>
2022-11-02 09:44:05 -0300

    recyclerView

--- TrackDelivery/app/src/main/java/com/miigubymia/trackdelivery/storage/ListFragment.kt
+++ TrackDelivery/app/src/main/java/com/miigubymia/trackdelivery/storage/ListFragment.kt
@@ -5,15 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.miigubymia.trackdelivery.R
 
 class ListFragment : Fragment() {
+
+    lateinit var listAdapter: ListAdapter
+    lateinit var recyclerView:RecyclerView
+    var listViewModel = ListViewModel()
+    var registers = arrayListOf<String>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        if (registers.isEmpty()){
+            val list = listViewModel.getListFromSharedPref(requireActivity())
+            if (list.isNotEmpty()){
+                registers.addAll(list)
+            }
+        }
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView = view.findViewById(R.id.recViewRegister)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        listAdapter = ListAdapter(registers)
+        recyclerView.adapter = listAdapter
     }
 }
